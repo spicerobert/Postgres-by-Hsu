775f383ae71b1aae8231d8c837c540a7a3a9d883
徐國堂 <roberthsu2003@gmail.com>
2025-08-03 08:11:33 +0000

    modify

--- lesson9/db.py
+++ lesson9/db.py
@@ -33,10 +33,3 @@ def get_all_stations():
     conn.close()
     return result
 
-def main():
-    stations = get_all_stations()
-    print("所有台鐵車站資訊的站點名稱：", stations)
-
-
-if __name__ == "__main__":
-    main()
