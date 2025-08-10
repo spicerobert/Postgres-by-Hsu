1f4a4cbed36eec3103b517df1734d817b2fea750
徐國堂 <roberthsu2003@gmail.com>
2025-08-10 07:49:34 +0000

    modify

---
+++ lesson10/main.py
@@ -0,0 +1,12 @@
+import source
+
+def main():
+    results = source.get_stations_names()
+    if results:
+        for station in results:
+            print(station)
+    else:
+        print("無法取得車站資料")
+
+if __name__ == "__main__":
+    main()
