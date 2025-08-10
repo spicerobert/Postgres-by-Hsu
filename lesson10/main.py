47f1937869b789da3a296500782a936b116b8692
徐國堂 <roberthsu2003@gmail.com>
2025-08-10 08:11:49 +0000

    modify

--- lesson10/main.py
+++ lesson10/main.py
@@ -1,12 +1,13 @@
 import datasource
+import streamlit as st
 
 def main():
+    st.title("台鐵車站名稱列表")
     results = datasource.get_stations_names()
     if results:
-        for station in results:
-            print(station)
+        st.dataframe(results, width=400, height=600)
     else:
-        print("無法取得車站資料")
+        st.error("無法取得車站資料")
 
 if __name__ == "__main__":
     main()
