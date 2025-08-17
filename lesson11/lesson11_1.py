1388c404d09bcdc49b208815d398da9367d826cc
徐國堂 <roberthsu2003@gmail.com>
2025-08-17 03:13:39 +0000

    modify

--- lesson11/lesson11_1.py
+++ lesson11/lesson11_1.py
@@ -7,5 +7,13 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 
-st.title("台鐵車站資訊")
-st.header("2023年各站進出人數")
+st.sidebar.title("台鐵車站資訊")
+st.sidebar.header("2023年各站進出人數")
+st.subheader("進出站人數顯示區")
+station = st.sidebar.selectbox(
+    "請選擇車站",
+    ("台北", "台中", "高雄"),
+)
+
+st.write("您選擇的車站:", station)
+
