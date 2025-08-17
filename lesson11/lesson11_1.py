acc4e2482b6d5fc8fbb4b78aa06b052b5409e650
徐國堂 <roberthsu2003@gmail.com>
2025-08-17 03:33:45 +0000

    modify

--- lesson11/lesson11_1.py
+++ lesson11/lesson11_1.py
@@ -1,18 +1,19 @@
-# 當輸出df變數時,st.write()會自動執行
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
-import pandas as pd
+import datasource
 
 st.sidebar.title("台鐵車站資訊")
 st.sidebar.header("2023年各站進出人數")
 st.subheader("進出站人數顯示區")
+
+@st.cache_resource
+def get_stations():
+    """取得車站資料"""
+    return datasource.get_stations_names()
+
+stations = get_stations()
 station = st.sidebar.selectbox(
     "請選擇車站",
-    ("台北", "台中", "高雄"),
+    stations,
 )
 
 st.write("您選擇的車站:", station)
